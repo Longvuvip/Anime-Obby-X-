a17ec61bbe809fe2a6dcf13dc6fd1381908c2792
Longvuvip <lnh851150@gmail.com>
2025-06-24 11:04:11 +0700

    anime obby x

---
+++ main.lua
@@ -0,0 +1,10 @@
+-- Anime Obby X - Main Script
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yourgithub/anime-obby-x/main/system.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yourgithub/anime-obby-x/main/character_system.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yourgithub/anime-obby-x/main/gamepass_handler.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yourgithub/anime-obby-x/main/win_system.lua"))()
+-- Tùy chọn thêm nhiều module khác tại đây nếu cần
+
+-- Script chính gọi các hệ thống chính
+print("Anime Obby X loaded successfully")
